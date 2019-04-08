46941421ee963ad1fdf6682076ae7ba375780d36
PYN <yxp7531540@gmail.com>
2019-04-08 09:27:41 -0500

    passed

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  return_value = nil
+  while i < collection.count
+    return_value = yield(collection[i])
+    i += 1
+  end
+  return_value
 end
+
+
+# binding.pry
+# 0
